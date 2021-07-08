21c0a80c9ed3ed7c4fea0b050ca23c7b1b0a610c
Luma Montes <lumagoesmontes@gmail.com>
2021-07-07 23:27:06 -0300

    iniciado estruturaçao de tabela

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,15 @@
+import "reflect-metadata";
 import express from "express";
+import "./database";
 
 const app = express();
 
 
-
-app.listen(3000, () => console.log('Server is runnig'));
+//  Tipos de parametrõs
+// Route Prams => http://localhost:300/prudots/12
+// Query Params => https://localhost:300/produtos?name=telcado
+//body params => {
+// "name": "teclado",
+// "description": "teclado bom"
+//}
+app.listen(3001, () => console.log('Server is runnig'));
